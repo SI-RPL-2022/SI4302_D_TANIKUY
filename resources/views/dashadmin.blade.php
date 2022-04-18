fff772fc99bb582ab483e4d23492140d6cd0f1e0
izungb <izungbrilian01@gmail.com>
2022-04-18 17:09:03 +0700

    add logout

--- resources/views/dashadmin.blade.php
+++ resources/views/dashadmin.blade.php
@@ -42,7 +42,15 @@
                                         <a class="dropdown-item" href="#!">Action</a>
                                         <a class="dropdown-item" href="#!">Another action</a>
                                         <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#!">Something else here</a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
                                     </div>
                                 </li>
                             </ul>
