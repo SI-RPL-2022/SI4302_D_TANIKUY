90f5e8cca2d8283f6229c67a777c483e16a56e34
DosaPemalas <habibalfajri11@gmail.com>
2022-04-19 16:06:11 +0700

    ubah front end assessment admin

--- resources/views/editsoal.blade.php
+++ resources/views/editsoal.blade.php
@@ -1,32 +1,34 @@
-@extends('layouts.main')
+@extends('layouts.admin')
 @section('container')
-                <div class="container d-flex justify-content-center" style='margin-top: 20px;'>
+                <div class="container">
+                    <div class="container d-flex justify-content-center" style='margin-top: 20px;'>
 
-                <div class="card" style="width: 40%; margin-bottom: 5px;">
-                  <form action="/editsoal2/{{$assessment->id}}" method="POST">
-                @csrf
-                  <div class="card-body">
-                    <div class="row p-2 m-2">
-                        <label for="nama_ass" class="form-label">Nama Assessemt</label>
-                        <input type="text" name="nama_ass" id="nama_ass" value="{{$assessment->nama_ass}}" class="form-control">
+                    <div class="card" style="width: 40%; margin-bottom: 5px;">
+                    <form action="/editsoal2/{{$assessment->id}}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <h3>Edit Assessment</h3>
+                        <hr>
+                        <div class="row p-2 m-2">
+                            <label for="nama_ass" class="form-label">Nama Assessemt</label>
+                            <input type="text" name="nama_ass" id="nama_ass" value="{{$assessment->nama_ass}}" class="form-control">
+                        </div>
+                        <div class="row p-2 m-2">
+                        	
+                            <label for="deskripsi" class="form-label">Deskripsi</label>
+                            <input type="text" name="deskripsi" id="deskripsi" value="{{$assessment->deskripsi}}" class="form-control">     
+                        </div>
+                        <div class="row p-2 m-2">
+                            
+                            <label for="soal" class="form-label">Soal</label>
+                            <input type="text" name="soal" id="soal" value="{{$assessment->soal}}" class="form-control">     
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-md">Submit</button>
+                        </div>
+                        </form>
                     </div>
-                    <div class="row p-2 m-2">
-                    	
-                        <label for="deskripsi" class="form-label">Deskripsi</label>
-                        <input type="text" name="deskripsi" id="deskripsi" value="{{$assessment->deskripsi}}" class="form-control">     
-                    </div>
-                    <div class="row p-2 m-2">
-                        
-                        <label for="soal" class="form-label">Soal</label>
-                        <input type="text" name="soal" id="soal" value="{{$assessment->soal}}" class="form-control">     
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-md">Submit</button>
-                	</div>
-                    </form>
                 </div>
 
-
-
-
-
 @endsection
+    </body>
+</html>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Admin Dashboard</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+        
+        <link href="{{asset('css/styles.css')}}" rel="stylesheet">
+    </head>
+    <body>
+
+
+@include('partials.navbaradmin');
+<div class="container">
+    @yield('container')
+</div>
+
+</body>
+</html>
