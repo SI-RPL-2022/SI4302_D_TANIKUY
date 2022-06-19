2a725816f7da347aac9e2c6c13f5986292dcab25
DESKTOP-BLB3DTM\titkb <izungbrilian01@gmail.com>
2022-06-19 15:56:17 +0700

    nambah gambar #ref18

--- resources/views/nilaiAssessments.blade.php
+++ resources/views/nilaiAssessments.blade.php
@@ -13,44 +13,7 @@
     <div class="container" style="width: 100%; margin-top:2px;">
       <div class="container py-4">
         <div class="row align-items-md-stretch">
-          <div class="col-md-3">
-            <div class="h-100 p-3 text-white rounded-3" style="background: #579924">
-              <ul class="list-unstyled ps-0">
-              <li class="mb-1 ">
-                <a class="btn btn-toggle align-items-center rounded collapsed text-light" href="{{ route('siswa.buy.course') }}" > Course
-                </a>
-                <div class="collapse show" id="home-collapse">
-                  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-light rounded">Overview</a></li>
-                    <li><a href="#" class="link-light rounded">Updates</a></li>
-                    <li><a href="#" class="link-light rounded">Reports</a></li>
-                  </ul>
-                </div>
-              </li>
-              <li class="mb-1">
-                <a class="btn btn-toggle align-items-center rounded collapsed text-light" href="{{ url('siswa/tugas') }}">
-                  Assesment
-                </a>
-              </li>
-              <li class="mb-1">
-                <a class="btn btn-toggle align-items-center rounded collapsed text-light" href="{{ url('siswa/tugas/jawaban') }}">
-                  Hasil Assessment
-                </a>
-              </li>
-              <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                  Langganan
-                </button>
-              </li>
-              <li class="border-top my-3"></li>
-                <li class="mb-1">
-                  <button class="btn btn-toggle align-items-center rounded collapsed text-light" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                    My Course
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
+        @include('layouts.sidebarSiswa')
 
           <div class="col-md-9">
             <div class="h-100 p-5 bg-light border rounded-3">
